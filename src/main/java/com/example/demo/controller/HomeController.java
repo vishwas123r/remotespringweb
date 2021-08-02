876c14cbea012beb15189ec1166bf1629343f8c1
vishwas123r <83428947+vishwas123r@users.noreply.github.com>
2021-08-02 18:37:42 +0530

    Update HomeController.java

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
     
     @GetMapping("/")
     public String home() {
-        return "Hello from Azure App Service! written with the spring boot application";
+        return "Hello from Azure App Service! written with the spring boot application and also thank you poonam for hosting";
     }
 }
